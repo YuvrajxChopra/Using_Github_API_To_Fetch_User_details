3da50a8f10bb4d31cc50c46adebd793df2409f78
Yuvraj Chopra <101337798+YuvrajxChopra@users.noreply.github.com>
2022-08-26 20:50:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,18 @@ document.getElementById('submit-btn').addEventListener('click', function(){
 
 document.getElementById('submit-btn').addEventListener('click', () =>{
     let username = document.getElementById('inputusername').value;
+    if(username.trim()==""){
+        return;
+    }
     let url = 'https://api.github.com/users/'+username;
     fetch(url).then(res=>res.json()).then( data =>{
-        
+        if(data.message=="Not Found"){
+            showhide(1);
+            document.getElementById('fullname').style.display='revert';
+            document.getElementById('fullname').innerHTML = "User don't exist!";
+            return;
+        }
+        showhide(0);
         document.getElementById('fullname').innerHTML = data.name;
         document.getElementById('username').innerHTML = "(@"+data.login+")";
         document.getElementById('userimg').src = data.avatar_url;
@@ -16,6 +25,7 @@ document.getElementById('submit-btn').addEventListener('click', () =>{
         
         console.log(data);
     }).catch(e=>{
+        showhide(1);
         console.log(e);
     })
 });
@@ -33,6 +43,30 @@ document.getElementById('submit-btn').addEventListener('click', () =>{
         document.getElementById('repogohere').appendChild(div);}
         console.log(data);
     }).catch(e=>{
+        showhide(1);
         console.log(e);
     })
 });
+
+
+
+function showhide(i){
+    if(i == 0){
+        document.getElementById('fullname').style.display='revert';
+        document.getElementById('username').style.display='revert';
+        document.getElementById('follwers').style.display='revert';
+        document.getElementById('repolist').style.display='revert';
+        document.getElementById('userphoto').style.display='revert';
+        document.getElementById('following').style.display='revert';
+        document.getElementById('pub-repos').style.display='revert';
+    }
+    else{
+        document.getElementById('repogohere').querySelectorAll('*').forEach( n => n.remove() );
+        document.getElementById('fullname').style.display='none';
+        document.getElementById('username').style.display='none';
+        document.getElementById('follwers').style.display='none';
+        document.getElementById('repolist').style.display='none';
+        document.getElementById('following').style.display='none';
+        document.getElementById('pub-repos').style.display='none';
+    }
+}
